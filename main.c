04b7764dfa62d54b928257b14fb7e8d7e95e6bd0
Achild0 <lesbats1u@univ-lorraine.fr>
2017-01-04 17:43:34 +0100

    Début de la fonction liregrille
    Ainsi que début d'une fonction débug.

--- main.c
+++ main.c
@@ -3,9 +3,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char
+
 void lireGrille(int tab,char fichiertxt[25])
 {
+   char nomfichier;
    
+   FILE* grille = NULL;
+   
+   grille =  fopen(fichiertxt,"r");
+   
+   while (grille == NULL)
+      {
+         printf("La grille n'a pas été trouvée, merci de vérifier le nom du fichier")
+         scanf("%s",nomfichier)
+         grille =  fopen(fichiertxt,"r");
+      }
+  
 }
 
 
