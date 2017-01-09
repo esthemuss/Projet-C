0d253483d425bc72b34e82f0aeef653ab3cf09af
esthemuss <mussot4u@univ-lorraine.fr>
2017-01-09 12:33:24 +0100

    ajout de la fonction estCandidat

--- util_func.c
+++ util_func.c
@@ -34,3 +34,67 @@ void lireGrille() //Faire l'en-tÃªte
    fclose(grille);
 
 }
+
+Cand estCandidat() //Faire l'entête
+{
+   int i,j,k, ligne, colonne;
+   int candligne, candcolonne, candcase;
+   candligne = 1;
+   candcolonne = 1;
+   candcase = 1;
+   nbc = 0;
+   tab = NULL;
+   
+      
+   while(NBO!=0)
+   { 
+     for(i=0;i<=8;i++)
+     {
+        for(j=0;j<=8;j++)
+        {      
+            if(G[i][j]=0)
+            {
+               for(k=1;k<=9;k++)
+               {    
+                  for(ligne=0;ligne<=8;ligne++)
+                  {
+                     if(k!=G[ligne][j])
+                        candligne = 0; 
+                  };
+                  
+                  for(colonne=0;colonne<=8;colonne++)
+                  {
+                     if(k!=G[i][colonne])
+                        candcolonne = 0;
+                  };
+                  
+                  if(i>=0 && i<=2 && j>=0 && j<=2)
+                  {  
+                  else if(i>=0 && i<=2 && j>=3 && j<=5)
+                     
+                  else if(i>=0 && i<=2 && j>=6 && j<=8)
+                     
+                  else if(i>=3 && i<=5 && j>=0 && j<=2)
+                     
+                  else if(i>=3 && i<=5 && j>=3 && j<=5)
+                     
+                  else if(i>=3 && i<=5 && j>=6 && j<=8)
+                     
+                  else if(i>=6 && i<=8 && j>=0 && j<=2)
+                     
+                  else if(i>=6 && i<=8 && j>=3 && j<=5)
+                     
+                  else if(i>=6 && i<=8 && j>=6 && j<=8)
+                     
+                  };  
+                  if(candligne=0 && candcolonne=0)
+                  {
+                     nbc = nbc++;
+                     tab = k;
+                  };
+               };
+            };
+         };
+      };
+   };      
+}
