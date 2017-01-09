5d2470267671b624b9ec58e9671056a332d886f1
Loïc <loic0314@gmail.com>
2017-01-09 12:07:56 +0100

    Update util_func.c

--- util_func.c
+++ util_func.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "util_func.h"
-#define TAILLE_MAX 3
+#define TAILLE_MAX 6
 
-void Debug(char msg)
+void Debug(char msg[])
 {
    printf("[DEBUG] : %s\n",msg);
 };
 
-void lireGrille() //Faire l'en-tête
+void main() //Faire l'en-tête
 {
    FILE* grille = NULL;
+   int x,y,element;
+   int i,j;
+   int G[9][9];
    char ligne[TAILLE_MAX];
-   char nomfichier[1000]; //le 1000 est à changer je pense
+   char nomfichier[100]; //le 100 est à changer je pense
    
    printf("Saisir le nom du fichier texte qui contient la grille.\n");
    scanf("%s",nomfichier);
@@ -25,12 +27,30 @@ void lireGrille() //Faire l'en-tête
       scanf("%s",nomfichier);
       grille =  fopen(nomfichier,"r");
    }
+   Debug("Début");
+   for (i=0;i<9;i++)
+   {
+      for (j=0;j<9;j++)
+      {
+         fscanf(grille, "%d %d %d", x, y, element);
+         if (x==i && y==j)
+            G[x][y]=element;
+         else
+            G[x][y]=0;
+      }  
+   }
+   Debug("Fin");
    
-   while (fgets(ligne, TAILLE_MAX, grille) != NULL)
+   /*
+   while (fscanf(grille, "%d %d %d", x, y, element) != EOF)
+      G[x][y]=element;
+      
+     
+    (fgets(ligne, TAILLE_MAX, grille) != NULL)
    {
       printf("%s", ligne);
    }
-   
+   */
    fclose(grille);
 
 }
